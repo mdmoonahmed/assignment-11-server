ca68db895b5d8c48b170c0db6b0cc9ffb660f53f
moonahmed <mdmoonahmedkhan10@gmail.com>
2025-12-08 22:43:38 +0600

    database connected

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = 3000
+
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.orfhois.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+ 
+    const db = client.db('chef hut');
+    const userCollection = db.collection('users');
+
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
